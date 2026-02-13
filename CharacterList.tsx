6d265b2c71232519d665a77047f637410903fd01
R2 studio <r2learn01@gmail.com>
2026-02-13 08:03:52 +0200

    CharacterList.tsx

--- CharacterList.tsx
+++ CharacterList.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
-import { CHARACTERS } from "./constants';
-import { CharacterId } from "./types';
+import { CHARACTERS } from './constants';
+import { CharacterId } from './types';
 
 interface CharacterListProps {
   activeId: CharacterId;
