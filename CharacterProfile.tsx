122e0d303dab7e5d9f022fd93ecb5f7315b56d79
R2 studio <r2learn01@gmail.com>
2026-02-13 08:04:10 +0200

    CharacterProfile.tsx

--- CharacterProfile.tsx
+++ CharacterProfile.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
-import { CharacterId } from "./types';
-import { CHARACTERS } from "./constants';
+import { CharacterId } from './types';
+import { CHARACTERS } from './constants';
 
 interface CharacterProfileProps {
   characterId: CharacterId;
